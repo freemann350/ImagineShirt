53151f1cfaa9c201e8592e1b2f723ed23fdb7ade
Alex G <alex.gaspar@live.com.pt>
2023-06-23 00:53:12 +0100

    policies-mgmt

--- routes/web.php
+++ routes/web.php
@@ -41,50 +41,55 @@ Route::controller(HomeController::class)->group(function () {
 
 Route::view('/checkout','checkout');
 
-//ORDERS ROUTES
-Route::controller(OrderController::class)->group(function () {
-    Route::get('/mgmt/pending-orders/', 'showPending')->name('orders.pending');
-    Route::get('/mgmt/order-history/', 'showHistory')->name('orders.history');
-    Route::patch('/mgmt/orders/{order}/status','changeStatus')->name('orders.status.change');
-    Route::get('/mgmt/order/view/{id}', 'show')->name('orders.show');
-    Route::resource('/mgmt/order', OrderController::class);
+Route::middleware('employee')->group(function () {
+    Route::get('/mgmt', [AdminController::class,'home'])->name('mgmt.home');
+
+    //ORDERS ROUTES
+    Route::controller(OrderController::class)->group(function () {
+        Route::get('/mgmt/pending-orders/', 'showPending')->name('orders.pending');
+        Route::get('/mgmt/order-history/', 'showHistory')->name('orders.history');
+        Route::patch('/mgmt/orders/{order}/status','changeStatus')->name('orders.status.change');
+        Route::get('/mgmt/order/view/{id}', 'show')->name('orders.show');
+        Route::resource('/mgmt/order', OrderController::class);
+    });
 });
 
 //ADMINISTRATION ROUTES
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/mgmt', 'home')->name('mgmt.home');
-    Route::get('/mgmt/statistics', 'statistic')->name('statistics');
-    Route::patch('/mgmt/users/{user}/blocked','changeBlock')->name('users.blocked.change');
-    Route::delete('mgmt/users/{user}','destroy')->name('users.destroy');
-    Route::resource('/mgmt/users',AdminController::class);
-});
+Route::middleware('admin')->group(function () {
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/mgmt/statistics', 'statistic')->name('statistics');
+        Route::patch('/mgmt/users/{user}/blocked','changeBlock')->name('users.blocked.change');
+        Route::delete('mgmt/users/{user}','destroy')->name('users.destroy');
+        Route::resource('/mgmt/users',AdminController::class);
+    });
 
 //CATEGORIES ROUTES
-Route::controller(CategoryController::class)->group(function () {
-    Route::delete('/mgmt/categories/{category}','destroy')->name('categories.destroy');
-    Route::resource('/mgmt/categories',CategoryController::class);
-});
+    Route::controller(CategoryController::class)->group(function () {
+        Route::delete('/mgmt/categories/{category}','destroy')->name('categories.destroy');
+        Route::resource('/mgmt/categories',CategoryController::class);
+    });
 
 //TSHIRTS ROUTES
-Route::controller(TshirtController::class)->group(function () {
-    Route::delete('/mgmt/tshirts/{tshirt}','destroy')->name('tshirts.destroy');
-    Route::get('/mgmt/tshirts/file/{file}','getPrivateFile')->name('tshirts.get.image'); //GET FOR PRIVATE IMAGES
-    Route::resource('/mgmt/tshirts',TshirtController::class);
-});
+    Route::controller(TshirtController::class)->group(function () {
+        Route::delete('/mgmt/tshirts/{tshirt}','destroy')->name('tshirts.destroy');
+        Route::get('/mgmt/tshirts/file/{file}','getPrivateFile')->name('tshirts.get.image'); //GET FOR PRIVATE IMAGES
+        Route::resource('/mgmt/tshirts',TshirtController::class);
+    });
 
 //COLOR ROUTES
-Route::controller(ColorController::class)->group(function () {
-    Route::delete('/mgmt/colors/{color}','destroy')->name('colors.destroy');
-    Route::resource('/mgmt/colors',ColorController::class);
-});
+    Route::controller(ColorController::class)->group(function () {
+        Route::delete('/mgmt/colors/{color}','destroy')->name('colors.destroy');
+        Route::resource('/mgmt/colors',ColorController::class);
+    });
 
 //PRICE ROUTES
-Route::resource('/mgmt/prices',PriceController::class)->only(['index','update']);
+    Route::resource('/mgmt/prices',PriceController::class)->only(['index','update']);
 
-//STAFF PASSWORD CHANGES
-Route::controller(UserController::class)->group(function () {
-    Route::get('/mgmt/changepassword','changePasswordStaff')->name('staff.changePassword');
-    Route::patch('/mgmt/changepassword/{user}/password','update')->name('staff.password.change');
+    //STAFF PASSWORD CHANGES
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/mgmt/changepassword','changePasswordStaff')->name('staff.changePassword');
+        Route::patch('/mgmt/changepassword/{user}/password','update')->name('staff.password.change');
+    });
 });
 
 //CUSTOMER ROUTES
